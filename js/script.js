c8a2ab3803fc430d2cbcf09db7afb7d6f04fde26
sitespersonalizadosMD <matheusrodrigues3001@gmail.com>
2026-07-04 15:25:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -759,24 +759,7 @@ if (imagemBermuda && selectBermuda) {
     });
 
 }
-/*=========================================
-BERMUDA MALHÃO
-=========================================*/
 
-// FOTO
-
-const imagemBermuda = document.getElementById("imagem-bermuda");
-const selectBermuda = document.getElementById("corBermuda");
-
-if (imagemBermuda && selectBermuda) {
-
-    selectBermuda.addEventListener("change", function () {
-
-        imagemBermuda.src = this.value;
-
-    });
-
-}
 /*=========================================
 BERMUDA MALHÃO - TAMANHO
 =========================================*/
